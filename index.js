87e7cb7f3737f5954ab3abf4a425f8870e259712
MagnusJnr <agumagnus5@gmail.com>
2024-09-15 19:06:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import sayMyColor from "./color.js";
+import sayMyCountry from "./country.js";
+import {sayMyFavFood, sayMyPet} from "./food.js";
+
+let color = sayMyColor
+let food = sayMyFavFood
+let country = sayMyCountry
+let pet = sayMyPet
+
+console.log(color, food, country, pet);
